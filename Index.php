8c9f8e6cbba7e429918a8fc200b5000f4cf5d880
Jakub <kubazlotek@gmail.com>
2020-09-07 23:40:55 +0200

    Update Index.php

--- Index.php
+++ Index.php
@@ -8,8 +8,23 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     exit;
 }
 
-// Include config file
-require_once "../config.php";
+@session_start();
+    $host = $_SERVER['HTTP_HOST'] . "/";
+    $inc = "/Includes/";
+    $serverside = "/Serverside/";
+    //$_SERVER['DOCUMENT_ROOT']
+
+    define("hostname", "localhost");
+    define("user", "root");
+    define("password","");
+    define("dbname", "testportal");
+    $conn = new mysqli(hostname, user, password, dbname);
+
+    $conn->set_charset("utf8");
+    if ($conn->connect_error) {
+        die("Connection failed: " .$conn->connect_error);
+    }
+
 
 // Define variables and initialize with empty values
 $username = $password = "";
