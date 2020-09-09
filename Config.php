b3ec5865ba75cb5a8783e37074f406acfb52440c
Daniel <daniel.kaminski123@gmail.com>
2020-09-09 21:48:39 +0200

    Remove Userdata and update Config.php

--- Config.php
+++ Config.php
@@ -2,13 +2,22 @@
 @session_start();
     $host = $_SERVER['HTTP_HOST'] . "/";
     $inc = "/Includes/";
-    $serverside = "/Serverside/";
+    $img = "/Img/";
+    $css = "/Styles/";
+    $admin = "/Admin/";
+    $serverside = "/ServerSide/";
     //$_SERVER['DOCUMENT_ROOT']
 
     define("hostname", "localhost");
     define("user", "root");
     define("password","");
     define("dbname", "testportal");
+    define("APP_DIR", $host);
+    define("APP_DIR_IMG", $host . $img);
+    define("APP_DIR_INC", $host . $inc);
+    define("APP_DIR_CSS", $host . $css);
+    define("APP_DIR_ADM", $host . $admin);
+
     $conn = new mysqli(hostname, user, password, dbname);
 
     $conn->set_charset("utf8");
