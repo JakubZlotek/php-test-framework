db821455b5d7cc3ffd35a0342478cbf83b2276b0
Daniel <daniel.kaminski123@gmail.com>
2020-09-09 21:53:43 +0200

    Update Nav.php

--- Includes/Nav.php
+++ Includes/Nav.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <img src="http://51.77.44.79/php-test-framework/img/logo.png" style = "height: 60px; width: 60px; margin-right: 20px;">
+  <img src="<? echo APP_DIR_IMG . "logo.png"?>" style = "height: 60px; width: 60px; margin-right: 20px;">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -7,7 +7,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="http://51.77.44.79/php-test-framework/index.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="<? echo APP_DIR . "index.php"?>">Home <span class="sr-only">(current)</span></a>
       </li>
 
       <?php
@@ -17,7 +17,7 @@
               Administrator
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="http://51.77.44.79/php-test-framework/Admin/Users.php">Użytkownicy</a>
+              <a class="dropdown-item" href="'. APP_DIR_ADM .'Users.php">Użytkownicy</a>
               <a class="dropdown-item" href="#">Another action</a>
             <div class="dropdown-divider"></div>
               <a class="dropdown-item" href="#">Something else here</a>
@@ -27,6 +27,6 @@
 
       ?>
     </ul>
-      <a class="btn btn-outline-success" href="http://51.77.44.79/php-test-framework/logout.php" role="button">Wyloguj się</a>
+      <a class="btn btn-outline-success" href="<? echo APP_DIR ?>logout.php" role="button">Wyloguj się</a>
   </div>
 </nav>
