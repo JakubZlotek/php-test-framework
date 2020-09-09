86fb7aa7185af12bafda84935b23d75ea096c30f
Daniel <daniel.kaminski123@gmail.com>
2020-09-09 22:03:44 +0200

    Update Nav.php

--- Includes/Nav.php
+++ Includes/Nav.php
@@ -1,7 +1,7 @@
 <? include '../Config.php' ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <img src="<? echo APP_DIR_IMG . "logo.png"?>" style = "height: 60px; width: 60px; margin-right: 20px;">
+  <img src="<? echo APP_DIR_IMG . 'logo.png'?>" style = "height: 60px; width: 60px; margin-right: 20px;">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
