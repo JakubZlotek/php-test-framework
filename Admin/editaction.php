afef7a64a301ca06883554109d198dda5c27db25
Jakub <kubazlotek@gmail.com>
2020-09-10 18:07:19 +0200

    dd

---
+++ Admin/editaction.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once "../config.php";
+
+    $id = trim($_POST["id"]);
+    if(!empty(trim($_POST["username"]))){
+      $username = trim($_POST["username"]);
+      $stmt = $conn->prepare("UPDATE users SET username = ? WHERE id = ?");
+      $stmt->bind_param("sss", $username, $id);
+      $stmt->execute();
+    };
+
+    if(!empty(trim($_POST["first_name"]))){
+      $imie = trim($_POST["first_name"]);
+      $stmt = $conn->prepare("UPDATE users SET name = ? WHERE id = ?");
+      $stmt->bind_param("sss", $imie, $id);
+      $stmt->execute();
+    };
+
+    if(!empty(trim($_POST["last_name"]))){
+      $nazwisko = trim($_POST["last_name"]);
+      $stmt = $conn->prepare("UPDATE users SET surname = ? WHERE id = ?");
+      $stmt->bind_param("sss", $nazwisko, $id);
+      $stmt->execute();
+    };
+
+    if(!empty(trim($_POST["email"]))){
+      $email = trim($_POST["email"]);
+      $stmt = $conn->prepare("UPDATE users SET email = ? WHERE id = ?");
+      $stmt->bind_param("sss", $email, $id);
+      $stmt->execute();
+    };
+
+
+    mysqli_close($conn);
+
+
+    ?>
