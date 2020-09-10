274d9aae171c74094207b3264a12f536b9e07715
Jakub <kubazlotek@gmail.com>
2020-09-10 21:14:42 +0200

    Update Nav.php

--- Includes/Nav.php
+++ Includes/Nav.php
@@ -1,7 +1,3 @@
-<?php
-require 'http://testy.jajuwa.pl/Config.php';
-?>
-
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <img src="http://testy.jajuwa.pl/Img/logo.png" style = "height: 60px; width: 60px; margin-right: 20px;">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
