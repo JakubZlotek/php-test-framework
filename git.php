1850e422e0d9d16963366abb665bf09db4dc789e
Jakub <kubazlotek@gmail.com>
2020-09-10 18:53:04 +0200

    dfd

--- git.php
+++ git.php
@@ -1,3 +1,4 @@
 <?php
-shell_exec('C:\Users\Administrator\Desktop\aktualizacja_repo.bat')
+$output = shell_exec('C:\Users\Administrator\Desktop\aktualizacja_repo.bat')
+echo "<pre>$output</pre>";
 ?>
