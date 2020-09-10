064b1aba9a6b3b6f80a6ae0dc07accea62be3b8a
Jakub <kubazlotek@gmail.com>
2020-09-10 18:59:22 +0200

    Update git.php

--- git.php
+++ git.php
@@ -1,3 +1,3 @@
 <?php
-shell_exec('cd C:\xampp\htdocs && git pull https://f79c32b345bc433f52bc2aa628579a20ce149c74@github.com/jakubzlotek/php-test-framework.git')
+shell_exec('cd C:\xampp\htdocs\php-test-framework && git pull https://f79c32b345bc433f52bc2aa628579a20ce149c74@github.com/jakubzlotek/php-test-framework.git')
 ?>
