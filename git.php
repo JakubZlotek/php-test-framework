705c650ba155875469a07a2660e0f7f23fa4c73f
Jakub <kubazlotek@gmail.com>
2020-09-10 19:05:26 +0200

    Update git.php

--- git.php
+++ git.php
@@ -1,7 +1,7 @@
 <?php
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-shell_exec('cd C:\xampp\htdocs\php-test-framework && git pull https://f79c32b345bc433f52bc2aa628579a20ce149c74@github.com/jakubzlotek/php-test-framework.git')
+shell_exec('cd C:\xampp\htdocs\php-test-framework && git pull https://f79c32b345bc433f52bc2aa628579a20ce149c74@github.com/jakubzlotek/php-test-framework.git');
 }
 
 ?>
