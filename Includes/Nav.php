88249f7787cbfac2d2d602f9d18aef3dc14439b3
EVBlue <daniel.kaminski123@gmail.com>
2020-09-10 00:33:24 -0700

    Update Nav.php

--- Includes/Nav.php
+++ Includes/Nav.php
@@ -16,14 +16,12 @@
       if ($_SESSION["isadmin"] == 1) {
           echo'<li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="fas fa-user-cog"></i>
               Administrator
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="http://testy.jajuwa.pl/Admin/Users.php">Użytkownicy</a>
+              <a class="dropdown-item" href="http://testy.jajuwa.pl/Admin/Users.php"><i class="fas fa-user"></i>Użytkownicy</a>
               <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
           </li>';
       }
 
