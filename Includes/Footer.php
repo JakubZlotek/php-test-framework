4adc08c6b453ad6fed02ac4f6ee09e94ea49892e
Jakub <kubazlotek@gmail.com>
2020-09-11 09:29:45 +0200

    d

--- Includes/Footer.php
+++ Includes/Footer.php
@@ -2,7 +2,7 @@
   <div class="row credits">
     &copy; Jakub Złotek & Daniel Kamiński
   </div>
-  <div class="row">
+  <div class="row themebackground">
     <div class="col-12 col-md">
       <img class="mb-2" src="http://testy.jajuwa.pl/Img/logo.png" alt="" width="24" height="24">
       <small class="d-block mb-3 text-muted">&copy; 2019-2020</small>
