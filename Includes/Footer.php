6c4c9f891240257d6b774c64ffd14c7c35e5193d
Jakub <kubazlotek@gmail.com>
2020-09-11 09:35:20 +0200

    cc

--- Includes/Footer.php
+++ Includes/Footer.php
@@ -1,22 +1,3 @@
-<footer class="pt-4 my-md-0 pt-md-5 border-top">
-  <div class="row credits">
-    &copy; Jakub Złotek & Daniel Kamiński
-  </div>
-  <div class="row themebackground">
-    <div class="col-12 col-md">
-      <img class="mb-2" src="http://testy.jajuwa.pl/Img/logo.png" alt="" width="24" height="24">
-      <small class="d-block mb-3 text-muted">&copy; 2019-2020</small>
-    </small>
-    </div>
-    <div class="col-6 col-md">
-      <h5>Features</h5>
-      <ul class="list-unstyled text-small">
-        <li><a class="text-muted" href="#">Cool stuff</a></li>
-        <li><a class="text-muted" href="#">Random feature</a></li>
-        <li><a class="text-muted" href="#">Team feature</a></li>
-        <li><a class="text-muted" href="#">Stuff for developers</a></li>
-        <li><a class="text-muted" href="#">Another one</a></li>
-        <li><a class="text-muted" href="#">Last time</a></li>
-      </ul>
-    </div>
+<footer class="footerstyle">
+cc
 </footer>
