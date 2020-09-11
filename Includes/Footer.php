867b3476bb263f631f309de9daef09758b8492d5
Jakub <kubazlotek@gmail.com>
2020-09-11 10:00:34 +0200

    d

--- Includes/Footer.php
+++ Includes/Footer.php
@@ -2,4 +2,20 @@
 <div class="credits">
   &copy; Jakub Złotek & Daniel Kamiński
 </div>
+<div class="row">
+<div class="col-md-6">
+sdsadas
+</div>
+
+<div class="col-md-6">
+  <ul>
+    <li>dupa1</li>
+    <li>dupa1</li>
+    <li>dupa1</li>
+    <li>dupa1</li>
+    <li>dupa1</li>
+  </ul>
+</div>
+
+</div>
 </footer>
