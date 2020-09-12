45476bd86a2598798c8d47e3e979f51728315842
Jakub <kubazlotek@gmail.com>
2020-09-12 13:17:43 +0200

    ff

--- Includes/Nav.php
+++ Includes/Nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light" >
+<nav class="navbar navbar-expand-lg navbar-light bg-light static-top mb-5 shadow" >
   <img src="http://testy.jajuwa.pl/Img/logo.png" style = "height: 60px; width: 60px; margin-right: 20px;">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
